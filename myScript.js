6d306f17a4b910f673133cbb49362c404b6327a1
Emily Nguyen <emilymylinh@gmail.com>
2016-11-18 17:40:09 -0800

    Removed offset function from myScript.js

--- myScript.js
+++ myScript.js
@@ -1,46 +1,6 @@
 /*global $ */
 /*jslint node: true*/
 
-
-
-function getWidth(obj){
-    var clone = obj.clone();
-    clone.css("visibility","hidden");
-    $('body').append(clone);
-    var width = clone.outerWidth();
-    clone.remove();
-    return width;
-}
-
-function getHeight(obj){
-    var clone = obj.clone();
-    clone.css("visibility","hidden");
-    $('body').append(clone);
-    var height = clone.outerHeight();
-    clone.remove();
-    return height;
-}
-
-function offset(pic, container) {
-    var width = getWidth(pic);
-    var height = getHeight(pic);
-    var cWidth = getWidth(container);
-    var cHeight = getHeight(container);
-    
-    if (width >= height) {
-        pic.css('height', cHeight);
-        width = getWidth(pic);
-        var offset = -(width - cWidth)/2;      
-        pic.css('margin-left', offset);
-    }
-    else {
-        pic.css('width', cWidth);
-        height = getHeight(pic);
-        var offset = -(height - cHeight)/2;      
-        pic.css('margin-top', offset);
-    }
-}
-
 function isIE() 
 {
     var ua = window.navigator.userAgent;
@@ -58,12 +18,6 @@ function isIE()
 
 var main = function () {
     $('body').css('background-color', '#f2f2f2');
-    
-    
-    // Center each portfolio img
-    $('#portfolio li').each(function () {
-        offset($(this).find('img'), $('.img-container'));
-    });
     
     // Resume circle animation
     $('.circle').hover(function () {
